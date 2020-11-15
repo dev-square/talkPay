95ed3563e6fceaee7425d9b0b5f42385999c3246
Davi <davidaj014@gmail.com>
2020-11-15 20:56:00 -0300

    IA pronta

---
+++ site/HTML/dinamico.php
@@ -0,0 +1,3 @@
+<?php
+echo "pagamento efetuado";
+?>
--- site/Script/comunicacoes.js
+++ site/Script/comunicacoes.js
@@ -1,10 +1,11 @@
 var Globalcontext = 0;
 var userSaid = '';
-var finalContext = 6;
+var finalContext = 8;
 var intentions = [];
 var presentIntent;
+
 //FUNÇÃO QUE FAZ A IA FALAR
-function say(phrase){
+function say(phrase,ouvir){
     var utter = new SpeechSynthesisUtterance();
         utter.text = phrase;
         utter.lang = 'pt-BR';
@@ -12,7 +13,9 @@ function say(phrase){
         window.speechSynthesis.speak(utter);
         //Falando...
         utter.onend = function() {
-            listen();
+            if (presentIntent.field.substr(0,4) == "form") 
+            document.getElementById(presentIntent.field).submit();
+            else if (ouvir == true) listen();
             //Escutando...
         }
 }
@@ -21,14 +24,14 @@ function conversation() {
     for(i = 1; i <= intentions.length-1;i++){
         if(intentions[i].context == Globalcontext && (intentions[i].trainingPhrase == userSaid || intentions[i].trainingPhrase == "any"))
         {
-            say(intentions[i].response);
+            say(intentions[i].response,true);
             presentIntent = intentions[i];
             Globalcontext = intentions[i].nextContext;
             break;
         }
         else if (i == intentions.length - 1 && Globalcontext != finalContext)
         {
-            say(intentions[0].response);
+            say(intentions[0].response,true);
             listen();
         }
     }
@@ -47,18 +50,13 @@ function listen(){
                 //final da fala do usuário
                 userSaid = event.results[i][0].transcript;
                 if (presentIntent.field != null && presentIntent.field != ""){
-                    if (presentIntent.field.substr(0,4) == "form"){
-                        say("Enviando");
-                        document.getElementById(presentIntent.field).submit();
-                    } else {
-                    say("Você disse " + userSaid);
+                    say("Você disse " + userSaid, false);
                     document.getElementById(presentIntent.field).value = userSaid;
-                    if (Globalcontext == 4) document.getElementById(presentIntent.field).value = '30,00';
-                    }
+                    if (Globalcontext == 5) document.getElementById(presentIntent.field).value = userSaid.substr(3,10);
                 }
                 conversation();
-                //document.getElementById("transcription2").textContent = userSaid;
-                //document.getElementById("context").textContent = "Contexto: " + Globalcontext;
+                document.getElementById("transcription2").textContent = userSaid;
+                document.getElementById("context").textContent = "Contexto: " + Globalcontext;
             }else{
                 userSaid += event.results[i][0].transcript;
             }
@@ -88,9 +86,12 @@ function intentMk(context,tp,response,nextContext = 0,field=""){
 }
 //INTENÇÕES
 intentMk(-1,'','Desculpe, não entendi');
-intentMk(0,'','Olá, denovo!. Prefere que eu liste os serviços ou tem um em mente?',1);
+intentMk(0,'','Olá!. Prefere que eu liste os serviços ou tem um em mente?',1);
 intentMk(1,'Liste os serviços','Você tem as opções de transferência, saque ou adicionar dinheiro',2);
 intentMk(2,'transferência','Deseja tranferir por  uma conta do mercado pago ou utilizar outro cartão?',3);
-intentMk(3,'mercado pago','Informe o e-mail da conta para a qual enviar o dinheiro',4,'txtEmail');
+intentMk(3,'Mercado Pago','Informe o e-mail da conta para a qual enviar o dinheiro',4,'txtEmail');
 intentMk(4,'any','informe o valor a ser enviado',5,'txtValue');
 intentMk(5,'any','diga uma mensagem para a conta creditada',6,'txtMsg');
+intentMk(6,'any','Tem certeza das informações prestadas?',7);
+intentMk(7,'sim','Enviando formulário',8,'form1');
+intentMk(7,'não','vamos revisar. Deseja tranferir por  uma conta do mercado pago ou utilizar outro cartão?',3);
