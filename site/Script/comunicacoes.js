3716f7d00220c65cb8813f1fc4904525a27caef9
Davi <davidaj014@gmail.com>
2020-11-15 22:53:47 -0300

    Adicionada função de processo em Intents

--- site/Script/comunicacoes.js
+++ site/Script/comunicacoes.js
@@ -9,7 +9,7 @@ function say(phrase,ouvir){
     var utter = new SpeechSynthesisUtterance();
         utter.text = phrase;
         utter.lang = 'pt-BR';
-        utter.rate = 1.4;
+        utter.rate = 2;
         window.speechSynthesis.speak(utter);
         //Falando...
         utter.onend = function() {
@@ -18,10 +18,6 @@ function say(phrase,ouvir){
             else {
                 //Condicional para falas que não são seguidas de uma entrada do usuário
                 if (ouvir == true) listen();
-                //
-                if (presentIntent.field.substr(0,5) == "trans"){
-                    document.getElementById(presentIntent.field.substr(5,presentIntent.field.length-5)).className = "hidden";
-                }
             }
             //Escutando...
         }
@@ -31,6 +27,8 @@ function conversation() {
     for(i = 1; i <= intentions.length-1;i++){
         if(intentions[i].context == Globalcontext && (intentions[i].trainingPhrase == userSaid || intentions[i].trainingPhrase == "any"))
         {
+            //EFETIVAÇÃO DA ETAPA DE PROCESSAMENTO DA INTENT
+            if (intentions[i].process != "") intentions[i].doProcess();
             say(intentions[i].response,true);
             presentIntent = intentions[i];
             Globalcontext = intentions[i].nextContext;
@@ -56,6 +54,7 @@ function listen(){
             if(event.results[i].isFinal){
                 //final da fala do usuário
                 userSaid = event.results[i][0].transcript;
+                
                 if (presentIntent.field != null && presentIntent.field != ""){
                     say("Você disse " + userSaid, false);
                     document.getElementById(presentIntent.field).value = userSaid;
@@ -81,22 +80,32 @@ class intent{
     response = "";
     nextContext = 0;
     field = "";
+    process="";
+
+    doProcess() {
+        switch(this.process){
+            case 'hideHome':
+                document.getElementById('card-visitors').className = "hidden";
+                break;
+        }
+    }
 }
-function intentMk(context,tp,response,nextContext = 0,field=""){
+function intentMk(context,tp,response,nextContext = 0,field="",process=""){
     Intent = new intent();
     Intent.context = context;
     Intent.trainingPhrase = [tp];
     Intent.response = response;
     Intent.field = field;
     Intent.nextContext = nextContext; 
+    Intent.process = process;
     intentions.push(Intent);
 }
 //INTENÇÕES
 intentMk(-1,'','Desculpe, não entendi');
 intentMk(0,'','Olá!. Prefere que eu liste os serviços ou tem um em mente?',1);
 intentMk(1,'Liste os serviços','Você tem as opções de transferência, saque ou adicionar dinheiro',2);
-intentMk(2,'transferência','Deseja tranferir por  uma conta do mercado pago ou utilizar outro cartão?',3,'transTranfer');
-intentMk(3,'Mercado Pago','Informe o e-mail da conta para a qual enviar o dinheiro',4,'txtEmail');
+intentMk(2,'transferência','Deseja tranferir por  uma conta do mercado pago ou utilizar outro cartão?',3);
+intentMk(3,'Mercado Pago','Informe o e-mail da conta para a qual enviar o dinheiro',4,'txtEmail','hideHome');
 intentMk(4,'any','informe o valor a ser enviado',5,'txtValue');
 intentMk(5,'any','diga uma mensagem para a conta creditada',6,'txtMsg');
 intentMk(6,'any','Tem certeza das informações prestadas?',7);
